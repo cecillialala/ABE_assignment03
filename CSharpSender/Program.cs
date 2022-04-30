6f663b473a52262f6c2047f7cf3332a240bb2b2f
Mahmoud-SK <mahmoudkarzoun00@gmail.com>
2022-04-30 17:25:19 +0200

    QuickChanges

--- CSharpSender/Program.cs
+++ CSharpSender/Program.cs
@@ -9,32 +9,33 @@ class Send
     public static void Main()
     {
         var factory = new ConnectionFactory() { HostName = "localhost" };
-        using(var connection = factory.CreateConnection())
-        using(var channel = connection.CreateModel())
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
         {
-            channel.QueueDeclare(queue: "hello",
+            channel.QueueDeclare(queue: "MQReservation",
                                  durable: false,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
 
 
-            var reservation = new Reservation(){
+            var reservation = new Reservation()
+            {
                 hotelId = 1,
                 checkIn = "2022-04-30",
                 checkOut = "2022-05-30",
                 roomNo = 223,
                 customerName = "Yuhu",
-                customerEmail= "Yuhu@Yuhu.dk",
+                customerEmail = "Yuhu@Yuhu.dk",
                 CustomerAddress = "Ravnsbjerg"
-            };        
+            };
 
             var message = JsonConvert.SerializeObject(reservation);
 
             var body = Encoding.UTF8.GetBytes(message);
 
             channel.BasicPublish(exchange: "",
-                                 routingKey: "hello",
+                                 routingKey: "MQReservation",
                                  basicProperties: null,
                                  body: body);
             Console.WriteLine(" [x] Sent {0}", message);
@@ -43,4 +44,4 @@ class Send
         Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
     }
-    }
+}
